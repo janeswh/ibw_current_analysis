2c56bce4a9222bc50142a100860394faaae2a156
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-05-25 12:28:55 -0400

    added temporarily ignored datasetes

--- file_settings.py
+++ file_settings.py
@@ -5,7 +5,7 @@ from enum import Enum
 class FileSettings(object):
 
     DATA_FOLDER = "/home/jhuang/Documents/phd_projects/injected_GC_data/data"
-    IGNORED = {"esc_unusable"}
+    IGNORED = {"p2_4wpi", "p2_5wpi", "p2_6wpi"}
     TABLES_FOLDER = (
         "/home/jhuang/Documents/phd_projects/injected_GC_data/tables"
     )
