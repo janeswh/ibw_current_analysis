dd019bd2f8af32afab7bd20cac667709e2f13605
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-05-25 12:35:54 -0400

    updated more ignored folder

--- file_settings.py
+++ file_settings.py
@@ -5,7 +5,7 @@ from enum import Enum
 class FileSettings(object):
 
     DATA_FOLDER = "/home/jhuang/Documents/phd_projects/injected_GC_data/data"
-    IGNORED = {"p2_4wpi", "p2_5wpi", "p2_6wpi"}
+    IGNORED = {"p2_4wpi", "p2_5wpi", "p2_6wpi", "extra_sweeps"}
     TABLES_FOLDER = (
         "/home/jhuang/Documents/phd_projects/injected_GC_data/tables"
     )
