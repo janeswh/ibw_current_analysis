5f2608bc60b469a61f2a3a061695517382bb9596
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-05-26 10:15:35 -0400

    increased post_stim and having calc_freq_peak use post_stim for window end

--- single_test.py
+++ single_test.py
@@ -1632,9 +1632,10 @@ class JaneCell(object):
         return avg_baseline_freq, std_baseline_freq
 
     def calculate_freq_peak(self, freq_df):
-
-        max_freq = freq_df.loc[self.stim_time :].max(axis=0)[0]
-        freq_peak_time = freq_df.loc[self.stim_time :].idxmax(axis=0)[0]
+        window_start = self.stim_time
+        window_end = self.stim_time + self.post_stim
+        max_freq = freq_df.loc[window_start:window_end].max(axis=0)[0]
+        freq_peak_time = freq_df.loc[window_start:window_end].idxmax(axis=0)[0]
         time_to_peak_freq = freq_peak_time - self.stim_time
 
         return max_freq, freq_peak_time, time_to_peak_freq
