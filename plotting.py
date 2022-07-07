fixed freq rise time calculation

plotting.py
@@ -834,16 +834,17 @@ def plot_freq_stats(dataset_freq_stats):
 
     datasets = dataset_freq_stats.keys()
     measures_list = [
-        ["Baseline-sub Peak Freq (Hz)", "Time to Peak Frequency (ms)"],
-        ["Baseline Frequency (Hz)", "Rise Time (ms)",],
+        "Baseline-sub Peak Freq (Hz)",
+        "Baseline Frequency (Hz)",
+        "Rise Time (ms)",
     ]
 
     freq_stats_fig = make_subplots(
-        rows=2,
-        cols=len(measures_list[0]),
+        rows=1,
+        cols=len(measures_list),
         # shared_xaxes=True,
-        horizontal_spacing=0.2,  # x_title="Timepoint"
-        vertical_spacing=0.10,
+        horizontal_spacing=0.1,  # x_title="Timepoint"
+        # vertical_spacing=0.10,
     )
     all_stats = pd.DataFrame()
     all_means = pd.DataFrame()
@@ -879,83 +880,80 @@ def plot_freq_stats(dataset_freq_stats):
         cell_mean_df = all_means.loc[all_means["Cell Type"] == cell_type]
         cell_sem_df = all_sems.loc[all_sems["Cell Type"] == cell_type]
 
-        for measure_list_ct, measure_sublist in enumerate(measures_list):
-            for measure_ct, measure in enumerate(measure_sublist):
+        for measure_ct, measure in enumerate(measures_list):
 
-                freq_stats_fig.add_trace(
-                    go.Box(
-                        x=cell_stats_df["Dataset"],
-                        y=cell_stats_df[measure],
-                        line=dict(color="rgba(0,0,0,0)"),
-                        fillcolor="rgba(0,0,0,0)",
-                        boxpoints="all",
-                        pointpos=0,
-                        marker_color=cell_type_bar_colors[cell_type],
-                        marker=dict(
-                            line=dict(
-                                color=cell_type_line_colors[cell_type], width=2
-                            ),
-                            size=15,
+            freq_stats_fig.add_trace(
+                go.Box(
+                    x=cell_stats_df["Dataset"],
+                    y=cell_stats_df[measure],
+                    line=dict(color="rgba(0,0,0,0)"),
+                    fillcolor="rgba(0,0,0,0)",
+                    boxpoints="all",
+                    pointpos=0,
+                    marker_color=cell_type_bar_colors[cell_type],
+                    marker=dict(
+                        line=dict(
+                            color=cell_type_line_colors[cell_type], width=2
                         ),
-                        name=cell_type,
-                        legendgroup=cell_type,
-                        offsetgroup=dataset_order[timepoint] + cell_type_ct,
+                        size=15,
                     ),
-                    row=measure_list_ct + 1,
-                    col=measure_ct + 1,
-                )
+                    name=cell_type,
+                    legendgroup=cell_type,
+                    offsetgroup=dataset_order[timepoint] + cell_type_ct,
+                ),
+                row=1,
+                col=measure_ct + 1,
+            )
 
-                # tries bar plot instead, plots mean of median with sem
-                freq_stats_fig.add_trace(
-                    go.Bar(
-                        x=cell_stats_df["Dataset"].unique(),
-                        y=cell_mean_df[measure],
-                        error_y=dict(
-                            type="data",
-                            array=cell_sem_df[measure],
-                            color=cell_type_line_colors[cell_type],
-                            thickness=3,
-                            visible=True,
-                        ),
-                        marker_line_width=3,
-                        marker_line_color=cell_type_line_colors[cell_type],
-                        marker_color=cell_type_bar_colors[cell_type],
-                        # marker=dict(markercolor=cell_type_colors[cell_type]),
-                        name=cell_type,
-                        legendgroup=cell_type,
-                        offsetgroup=dataset_order[timepoint] + cell_type_ct,
+            # tries bar plot instead, plots mean of median with sem
+            freq_stats_fig.add_trace(
+                go.Bar(
+                    x=cell_stats_df["Dataset"].unique(),
+                    y=cell_mean_df[measure],
+                    error_y=dict(
+                        type="data",
+                        array=cell_sem_df[measure],
+                        color=cell_type_line_colors[cell_type],
+                        thickness=3,
+                        visible=True,
                     ),
-                    row=measure_list_ct + 1,
-                    col=measure_ct + 1,
-                )
+                    marker_line_width=3,
+                    marker_line_color=cell_type_line_colors[cell_type],
+                    marker_color=cell_type_bar_colors[cell_type],
+                    # marker=dict(markercolor=cell_type_colors[cell_type]),
+                    name=cell_type,
+                    legendgroup=cell_type,
+                    offsetgroup=dataset_order[timepoint] + cell_type_ct,
+                ),
+                row=1,
+                col=measure_ct + 1,
+            )
 
-                if measure == "Mean Trace Peak (pA)":
-                    freq_stats_fig.update_yaxes(
-                        autorange="reversed",
-                        row=measure_list_ct + 1,
-                        col=measure_ct + 1,
-                    )
-
-                #  below is code from stack overflow to hide duplicate legends
-                names = set()
-                freq_stats_fig.for_each_trace(
-                    lambda trace: trace.update(showlegend=False)
-                    if (trace.name in names)
-                    else names.add(trace.name)
-                )
+            if measure == "Mean Trace Peak (pA)":
                 freq_stats_fig.update_yaxes(
-                    title_text="Peak Frequency (Hz)"
-                    if measure == "Baseline-sub Peak Freq (Hz)"
-                    else measure,
-                    row=measure_list_ct + 1,
-                    col=measure_ct + 1,
+                    autorange="reversed", row=1, col=measure_ct + 1,
                 )
-                freq_stats_fig.update_xaxes(
-                    categoryorder="array",
-                    categoryarray=list(dataset_order.keys()),
-                    row=measure_list_ct + 1,
-                    col=measure_ct + 1,
-                )
+
+            #  below is code from stack overflow to hide duplicate legends
+            names = set()
+            freq_stats_fig.for_each_trace(
+                lambda trace: trace.update(showlegend=False)
+                if (trace.name in names)
+                else names.add(trace.name)
+            )
+            freq_stats_fig.update_yaxes(
+                title_text="Peak Frequency (Hz)"
+                if measure == "Baseline-sub Peak Freq (Hz)"
+                else measure,
+                row=1,
+                col=measure_ct + 1,
+            )
+            freq_stats_fig.update_xaxes(
+                categoryorder="array",
+                categoryarray=list(dataset_order.keys()),
+                row=1,
+                col=measure_ct + 1,
+            )
 
     freq_stats_fig.update_layout(
         boxmode="group",
@@ -3039,28 +3037,37 @@ def plot_annotated_freq(df, stats, dataset):
     else:
         stim_time = p14_acq_parameters.STIM_TIME
 
-    rise_window = df.loc[stim_time:]
+    # rise_window = df.loc[stim_time:]
 
-    root = rise_window.loc[stim_time][0]
-    rise_window = rise_window - root
+    # root = rise_window.loc[stim_time][0]
+    # rise_window = rise_window - root
 
-    df = df - root
+    # df = df - root
+
     peak = stats["Peak Frequency (Hz)"][0]
-    root_sub_peak = peak - root
     sub_peak = stats["Baseline-sub Peak Freq (Hz)"][0]
     peak_time = stats["Peak Frequency Time (ms)"][0]
     time_to_peak = stats["Time to Peak Frequency (ms)"][0]
     baseline_freq = stats["Baseline Frequency (Hz)"][0]
-    rise_time = stats["Rise Time (ms)"][0]
+    # rise_time = stats["Rise Time (ms)"][0]
 
-    rise_start_idx = np.argmax(rise_window >= root_sub_peak * 0.2)
-    rise_end_idx = np.argmax(rise_window >= root_sub_peak * 0.8)
+    # calculate root point for baseline
+    root_start = 350
+    root_window = df[root_start:peak_time]
+    root_time = root_window.idxmin()[0]
+    root = root_window.min()
 
-    rise_start = rise_window.index[rise_start_idx]
-    rise_end = rise_window.index[rise_end_idx]
+    response_window = df[root_time:peak_time]
 
-    rise_start_freq = rise_window.loc[rise_start][0]
-    rise_end_freq = rise_window.loc[rise_end][0]
+    rise_start_idx = np.argmax(response_window >= peak * 0.2)
+    rise_end_idx = np.argmax(response_window >= peak * 0.8)
+
+    rise_start = response_window.index[rise_start_idx]
+    rise_end = response_window.index[rise_end_idx]
+    rise_time = rise_end - rise_start
+
+    rise_start_freq = df.loc[rise_start][0]
+    rise_end_freq = df.loc[rise_end][0]
 
     layout = go.Layout(plot_bgcolor="rgba(0,0,0,0)")
     fig = go.Figure(layout=layout)

single_test.py
@@ -1357,7 +1357,7 @@ class JaneCell(object):
         events_fig.show()
         self.events_fig = events_fig
 
-    def analyze_avg_frequency(self, bin_width=10):
+    def analyze_avg_frequency(self, bin_width=10, example=False):
         """
         bin_width is width of bins in ms
         time_stop is time of last timepoint wanted for BARS, in ms
@@ -1432,6 +1432,7 @@ class JaneCell(object):
             y_array=freq,
             x_plot=windowed_x_plot,
             smoothed=windowed_raw_avg_frequency,
+            rise_points=True if example == False else False,
         )
 
         # raw_avg_frequency = run_BARS_smoothing(
@@ -1679,9 +1680,11 @@ class JaneCell(object):
         root_time=None,
         data_type=None,
     ):
-        # do root subtraction for events
+        response_window = response_window[root_time:peak_time]
+        if data_type == "frequency":
+            root = response_window.loc[root_time][0]
+
         if data_type == "event":
-            response_window = response_window[root_time:peak_time]
             root = response_window[root_time]
             response_window = response_window - root
             peak = peak - root
@@ -1968,11 +1971,22 @@ class JaneCell(object):
             if (max_freq > 1) and (
                 freq_peak_time <= self.stim_time + self.post_stim
             ):
+
+                # calculate root time to start finding rise_start
+                root_window_start = (
+                    p14_acq_parameters.TP_START + p14_acq_parameters.TP_LENGTH
+                )
+                root_window = self.avg_frequency_df.loc[
+                    root_window_start:freq_peak_time
+                ]
+                root_time = root_window.idxmin()[0]
+
                 rise_time, rise_start, rise_end = self.calculate_rise_time(
                     max_freq,
                     freq_peak_time,
-                    response_window,
+                    root_window,
                     polarity="+",
+                    root_time=root_time,
                     data_type="frequency",
                 )
 
@@ -1998,6 +2012,8 @@ class JaneCell(object):
                 "Baseline Frequency (Hz)": avg_baseline_freq,
                 "Baseline-sub Peak Freq (Hz)": max_freq - avg_baseline_freq,
                 # "Response Onset Latency (ms)": onset_time - self.stim_time,
+                "20% Rise Start (ms)": rise_start,
+                "80% Rise End (ms)": rise_end,
                 "Rise Time (ms)": rise_time,
                 "Decay tau": tau,
             },
@@ -2008,7 +2024,14 @@ class JaneCell(object):
         self.avg_frequency_stats = avg_freq_stats
 
     def plot_annotated_freq_histogram(
-        self, event_times, x_stop, x_array, y_array, x_plot, smoothed
+        self,
+        event_times,
+        x_stop,
+        x_array,
+        y_array,
+        x_plot,
+        smoothed,
+        rise_points=False,
     ):
         """
         Makes raster plot + annotated freq histogram, with decay fit if
@@ -2138,12 +2161,45 @@ class JaneCell(object):
             go.Scatter(
                 x=[self.avg_frequency_stats["Peak Frequency Time (ms)"][0]],
                 y=[self.avg_frequency_stats["Peak Frequency (Hz)"][0]],
-                marker=dict(color="#FFB233", size=12),
+                marker=dict(color="#FFB233", size=10),
                 name="peak frequency",
             ),
             row=2,
             col=1,
         )
+
+        if rise_points is True and self.condition == "light":
+            # add 20% rise start, 80% rise end
+            rise_start_time = self.avg_frequency_stats["20% Rise Start (ms)"][
+                0
+            ]
+            rise_end_time = self.avg_frequency_stats["80% Rise End (ms)"][0]
+            if np.isnan(rise_start_time) == False:
+
+                rise_start_idx = np.where(x_plot == rise_start_time)[0][0]
+                rise_end_idx = np.where(x_plot == rise_end_time)[0][0]
+
+                annotated_freq.add_trace(
+                    go.Scatter(
+                        x=[rise_start_time],
+                        y=[smoothed[rise_start_idx]],
+                        marker=dict(color="gray", size=8),
+                        name="20% Rise start",
+                    ),
+                    row=2,
+                    col=1,
+                )
+
+                annotated_freq.add_trace(
+                    go.Scatter(
+                        x=[rise_end_time],
+                        y=[smoothed[rise_end_idx]],
+                        marker=dict(color="gray", size=8),
+                        name="80% Rise end",
+                    ),
+                    row=2,
+                    col=1,
+                )
 
         # plots mean trace with annotations
         window_toplot = self.sub_mean_trace[
@@ -2174,6 +2230,7 @@ class JaneCell(object):
             row=3,
             col=1,
         )
+
         annotated_freq.update_yaxes(
             title_text="Current (pA)", row=3, col=1,
         )